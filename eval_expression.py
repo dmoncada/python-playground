15d0eb0e9fc39aef43a25115774e396a37ede45e
David Moncada <david_moncada@icloud.com>
2017-10-08 08:01:19 -0500

    Added postfix evaluation, .gitignore

--- eval_expression.py
+++ eval_expression.py
@@ -3,9 +3,9 @@
 from stack import Stack
 
 
-def eval_expression(expression):
+def eval_infix(expression):
     '''
-    Evaluates a (simple) mathematical expression such as 9 + 2 * ( 7 - 3 / 4 )
+    Evaluates an infix mathematical expression such as 9 + 2 * ( 7 - 3 / 4 )
     using Dijkstra's two-stack algorithm. Taken from Lee's and Hubbard's book
     [1].
 
@@ -33,18 +33,7 @@ def eval_expression(expression):
             top_op = ops.pop()
 
             if top_op in ['+', '-', '*', '/']:
-                num2 = nums.pop()
-                num1 = nums.pop()
-
-                if top_op == '/' and num2 == 0:
-                    raise ZeroDivisionError('division by zero is undefined')
-
-                nums.push({
-                    '+': lambda a, b: a + b,
-                    '-': lambda a, b: a - b,
-                    '*': lambda a, b: a * b,
-                    '/': lambda a, b: a / b
-                }[top_op](num1, num2))
+                do_operation(top_op, nums)
 
             elif top_op == '(' and op == ')':
                 return
@@ -54,11 +43,9 @@ def eval_expression(expression):
     ops = Stack()
     nums = Stack()
 
-    # Initially, a left paren is pushed on the operators stack.
+    # Initially, a left paren is pushed on the ops. stack.
     ops.push('(')
 
-    # 1. If the token is an operator, we must operate on the two stacks.
-    # 2. If the token is a number, push it on the numbers stack.
     for token in expression:
         if token in ['(', ')', '+', '-', '*', '/']:
             operate(token, ops, nums)
@@ -68,19 +55,62 @@ def eval_expression(expression):
     # We operate on the stacks one last time with an extra right paren.
     operate(')', ops, nums)
 
-    # The operators stack is now empty and numbers stack holds the result.
     return nums.pop()
 
 
+def eval_postfix(expression):
+    '''
+    Evaluates a postfix (a.k.a. reverse Polish) mathematical expression such as
+    1 2 - 4 5 + * using a single stack. Translated to Python from [1] by K&R.
+
+    Notice the lack of parens in the expression; there's no ambiguity in the
+    order of evaluation as long each operator receives exactly the number of
+    operands it expects.
+
+    The same caveats for the infix expression evaluator fuction apply here.
+
+    [1] The C Programming Language
+
+    '''
+
+    nums = Stack()
+
+    for token in expression:
+        if token in ['+', '-', '*', '/']:
+            do_operation(token, nums)
+        else:
+            nums.push(int(token))
+
+    return nums.pop()
+
+
+# Here is where ops. are actually performed.
+def do_operation(op, nums):
+    num2 = nums.pop()
+    num1 = nums.pop()
+
+    if op == '/' and num2 == 0:
+        raise ZeroDivisionError('division by zero is undefined')
+
+    nums.push({
+        '+': lambda a, b: a + b,
+        '-': lambda a, b: a - b,
+        '*': lambda a, b: a * b,
+        '/': lambda a, b: a / b
+    }[op](num1, num2))
+
+
 if __name__ == '__main__':
-    expr1 = '3 + 4 * 2 / ( 2 - 3 )'
-    expr2 = '9 + 2 * ( 7 - 3 / 4 )'
-    expr3 = '( 2 + 3 * 4 ) * 5 + 1'
+    infix1 = '3 + 4 * 2 / ( 2 - 3 )'
+    infix2 = '9 + 2 * ( 7 - 3 / 4 )'
+    infix3 = '( 2 + 3 * 4 ) * 5 + 1'
 
-    ans1 = eval_expression(expr1.split(' '))
-    ans2 = eval_expression(expr2.split(' '))
-    ans3 = eval_expression(expr3.split(' '))
+    expressions = [infix1, infix2, infix3]
 
-    print(f'The evaluation of: {expr1} is: {ans1}')
-    print(f'The evaluation of: {expr2} is: {ans2}')
-    print(f'The evaluation of: {expr3} is: {ans3}')
+    for expression in expressions:
+        ans = eval_infix(expression.split(' '))
+        print(f'The evaluation of: {expression} is: {ans}')
+
+    postfix = '1 2 - 4 5 + *'
+    ans = eval_postfix(postfix.split(' '))
+    print(f'The evaluation of: {postfix} is: {ans}')
